9d90f3d7cba6888b168d28de8715ef75e53f2e07
Sriramkod <kodipakasriram@gmail.com>
2019-04-06 15:55:57 +0530

    about the application

---
+++ app/src/main/java/com/example/ksriram/food_in_hands/about.java
@@ -0,0 +1,24 @@
+package com.example.ksriram.food_in_hands;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class about extends AppCompatActivity {
+Button order;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+order=(Button)findViewById(R.id.order);
+order.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+      Intent intent = new Intent(about.this, gridactivity.class);
+        startActivity(intent);
+    }
+});
+    }
+}
